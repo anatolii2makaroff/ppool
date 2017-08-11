329c3cce5aba7e8710eecb17f5a06b2726841e08
amakarov <amakarov@npoprogress.com>
2017-08-11 18:19:38 +0300

    ..

--- src/main.erl
+++ src/main.erl
@@ -2,6 +2,22 @@
 -module(main).
 -export([start/0]).
 
+
+
+for(N, F)
+    when N>0->
+    F(),
+    if
+        N=:=2 -> timer:sleep(2000);
+        true -> ok
+    end,
+
+    for(N-1, F);
+
+for(0, _) ->
+    ok.
+
+
 start() ->
     application:start(ppool),
     ppool:start_pool(my, 10, {worker, start_link, []}),
@@ -9,20 +25,11 @@ start() ->
     ppool_worker:start_worker(my),
     ppool_worker:start_worker(my),
 
-
-    ppool_worker:call_all_workers(my, 1000),
-    ppool_worker:call_all_workers(my, 1000),
-
-    ppool_worker:call_all_workers(my, 1000),
-    ppool_worker:call_all_workers(my, 1000),
-
-    ppool_worker:call_all_workers(my, 1000),
-    ppool_worker:call_all_workers(my, 1000),
-
-    ppool_worker:call_all_workers(my, 1000),
-    ppool_worker:call_all_workers(my, 1000),
-
-
+    for(12, fun() -> 
+                    ppool_worker:call_all_workers(my, 100)
+            end
+    ),
+ 
 
     ok.
 
