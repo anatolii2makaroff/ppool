9072e235a96db7effe6a26ccd47c062af10d1f13
amakarov <amakarov@npoprogress.com>
2017-09-12 18:07:46 +0300

    add docker examples

--- src/main.erl
+++ src/main.erl
@@ -1,92 +1,60 @@
 
 -module(main).
 -export([start/0
+        ,cmd/3
+        ,start_all_workers/0
+        ,start_stream_workers/0
+        ,start_map_workers/0
         ,call_sync_workers/0
         ,call_worker/0
         ,sub_all/0
         ,sub_one/0
-        ,stream/0
         ]).
-
-
-
-for(N, F)
-    when N>0->
-    F(),
-    if
-        N=:=-9 -> % ppool_worker:cast_all_workers(my,10000),
-                  timer:sleep(2000);
-        true -> ok
-    end,
-
-    for(N-1, F);
-
-for(0, _) ->
-    ok.
 
 
 start() ->
         application:start(node_watch),
+        application:start(node_scheduler),
         application:start(ppool),
-        ppool:start_pool(my, 10, {port_worker, start_link, []}),
-        ppool:start_pool(my2, 10, {port_worker, start_link, []}),
-        ppool:start_pool(my3, 10, {port_worker, start_link, []}),
-        ppool:start_pool(stream, 2, {port_worker, start_link, []}),
 
+        ppool:start_pool(python, 10, {port_worker, start_link, []}),
+        ppool:start_pool(nodejs, 10, {port_worker, start_link, []}),
+        ppool:start_pool(python_stream, 2, {port_worker, start_link, []}),
 
-
-
-        % ppool_worker:start_all_workers(my, "python ./priv/hello.py 2 2"
-        %                                    ">> ./logs/hello_err.log"),
-
-        % Cmd = "sudo docker run --rm -i -u python -w /home/python/ -v /tmp/:/tmp/ pyhello:0.1 /usr/bin/python hello.py 2 2>>/tmp/hello.log",
-
-        Cmd = "python ./priv/hello.py 2 2>>/tmp/hello.log",
-
-
-
-
-        Workers = [Cmd,
-                   Cmd
-                   %%"nodejs ./priv/hello.js 2>> ./logs/hello_err_js.log"
-                  ],
-
-        ppool_worker:start_map_workers(my, Workers),
-        ppool_worker:start_map_workers(my2, Workers),
-        ppool_worker:start_map_workers(my3, Workers),
-
-
-
-
-
-        
-        %for(10, fun() -> 
-        %                ppool_worker:call_workers(my, "{\"in\":2}\n")
-        %        end
-        %),
-    
 
         ok.
 
 
-stream() ->
-        Workers = ["python ./priv/hello_stream.py 1 2>> ./logs/hello_err.log",
-                   "python ./priv/hello_stream.py 2 2>> ./logs/hello_err.log"
-                   %%"nodejs ./priv/hello.js 2>> ./logs/hello_err_js.log"
-                  ],
-
-        ppool_worker:start_map_workers(stream, Workers),
+start_all_workers() ->
+    ppool_worker:start_all_workers(python, cmd("hellopy:0.1.0",
+                                               "python hello.py 1",
+                                               "hello.log")).
 
 
-    ppool_worker:stream_all_workers(stream, "{\"in\":2}\n"). 
+start_stream_workers() ->
+    ppool_worker:start_all_workers(python_stream, cmd("hellopy:0.1.0",
+                                               "python hello_stream.py 1",
+                                               "hello_stream.log")).
+
+
+
+start_map_workers() ->
+
+    Cmds=[cmd("hellojs:0.1.0","nodejs hello.js", "hellojs.log")
+         ,cmd("hellojs:0.1.0","nodejs hello.js", "hellojs.log")
+         ,cmd("hellojs:0.1.0","nodejs hello.js", "hellojs.log")
+         ],
+
+    ppool_worker:start_map_workers(nodejs, Cmds).
+
 
 
 call_sync_workers() ->
-    ppool_worker:call_sync_workers(my, "{\"in\":2}\n").
+    ppool_worker:call_sync_workers(python, "hello python\n").
 
 
 call_worker() ->
-    ppool_worker:call_worker(my, "{\"in\":2}\n").
+    ppool_worker:call_worker(python, "hello python\n").
 
 
 
@@ -100,7 +68,19 @@ sub_one() ->
 
 
 
+cmd(Img, Cmd, L) ->
+   R = lists:concat(["sudo docker run --rm -i -u drop -w /home/drop/"
+                     " -v /tmp/:/tmp/ ", Img, " ", Cmd, 
+                     " 2>>/tmp/", L]),
+   R.
 
 
- 
+for(N, F)
+    when N>0->
+    F(),
+
+    for(N-1, F);
+
+for(0, _) ->
+    ok.
 
