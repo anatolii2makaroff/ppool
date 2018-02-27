9efb02d2d88b84a9518d9ee45d7d9d181611aacf
anatolii2makaroff <anatolii.makaroff@gmail.com>
2018-02-27 12:05:06 +0300

    add to flower_sc.. ipvs

---
+++ priv/flower_sc_stream/float_ip.py
@@ -0,0 +1,96 @@
+#
+# VIP + IPVS
+#
+# os ENVI
+#      DROP_VIP - Virtual ip adress
+#      DROP_VIP_IFACE - interface
+
+
+import os
+import sys
+import time
+import subprocess as sp
+
+
+def log(m):
+    sys.stderr.write("{}: {}\n".format(time.time(), m))
+    sys.stderr.flush()
+
+_API_PORT = 8081
+
+#  VIP cmds ####
+
+_VIP_CMDS = [
+    "ifconfig lo:0 down",
+    "ifconfig {iface}:0 {vip} netmask 255.255.255.255 broadcast {vip}",
+    "iptables -F",
+    "ipvsadm -C",
+    "ipvsadm -A -t {vip}:{port} -s rr"
+]
+
+_ADD_RSERV = "ipvsadm -a -t {vip}:{port} -r {serv}:{port} -g"
+
+################
+
+_RSERV_CMDS = [
+    "ifconfig {iface}:0 down",
+    "ifconfig lo:0 {vip} netmask 255.255.255.255 broadcast {vip}",
+    "iptables -F",
+    "ipvsadm -C"
+    # "route add -host {vip} dev lo:0"
+]
+
+
+def make_vip(r_servers, is_vip=False):
+    """
+    r_servers - list of real servers
+    """
+    _vip = os.environ.get("DROP_VIP")
+    _iface = os.environ.get("DROP_VIP_IFACE")
+
+    if _vip is None or _iface is None:
+        log("WARN: not set environ DROP_VIP & DROP_VIP_IFACE..{}/{}/{}".format(_vip, _iface))
+        return
+
+    log("start make_vip: {} {}".format(is_vip, r_servers))
+
+    try:
+        if is_vip:
+            
+            for c in _VIP_CMDS:
+                _cmd = c.format(**{"iface": _iface,
+                                 "vip": _vip,
+                                 "port": _API_PORT
+                                 })
+
+                log(_cmd)
+                log(sp.check_output("{};exit 0".format(_cmd),
+                                    shell=True, stderr=sp.STDOUT))
+            for r in r_servers:
+                _cmd = _ADD_RSERV.format(**{"vip": _vip,
+                                          "port": _API_PORT,
+                                          "serv": r
+                                          })
+                log(_cmd)
+                log(sp.check_output("{};exit 0".format(_cmd),
+                                    shell=True, stderr=sp.STDOUT))
+        else:
+            # real server
+            for c in _RSERV_CMDS:
+                _cmd = c.format(**{"iface": _iface,
+                                 "vip": _vip
+                                 })
+                log(_cmd)
+                log(sp.check_output("{};exit 0".format(_cmd),
+                                    shell=True, stderr=sp.STDOUT))
+
+    except Exception as e:
+        log(e)
+
+    return
+
+if __name__ == "__main__":
+    make_vip(["host1",
+              "host2"
+              ],
+             True)
